971c4c3946cc0dcf4b50f9252b054b44f1a09a2f
Yamuna7869 <156390517+Yamuna7869@users.noreply.github.com>
2024-01-13 07:48:57 +0530

    Update my second file.java
    updating data in repodemo2

--- my second file.java
+++ my second file.java
@@ -1,6 +1,11 @@
 public class RepoDemo2
 {
   int i=5;
-  static j=10;
+  static int j=10;
   i=j;
+public static void main (String[] args)
+  {
+  System.out.println(i);
+  System.out.println(j);
+}
 }
